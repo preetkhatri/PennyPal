6cc5e862e9e33c7d5667741e22aea691ff939499
preetkhatri <preetkhatri07@gmail.com>
2024-04-15 02:38:46 -0700

    Few changes here and there

--- frontend/src/components/cards/Cards.jsx
+++ frontend/src/components/cards/Cards.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import "./cards.css"
+import axios from "axios";
 import Common from "../../common/Common"
 import { IncomeContext } from "../../Context/IncomeContext";
 import { ExpenseContext } from "../../Context/ExpenseContext";
@@ -9,26 +10,56 @@ import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 const Cards = () => {
 
   const { totalExpense, useTotalExpense } = useContext(ExpenseContext);
-  const { totalIncome, useTotalIncome } = useContext(IncomeContext)
+  const { totalIncome, useTotalIncome } = useContext(IncomeContext);
+
+
+  const [inc, setInc] = React.useState(0);
+  const [exp, setExp] = React.useState();
+
+  useEffect(() => {
+    const fetchIncomes = async () => {
+      let total = 0;
+      const incResp = await axios.get("http://localhost:5000/api/v1/get-incomes");
+      const inc = incResp.data;
+      inc.forEach((key) => {
+        total = total + key.amount;
+      })
+      setInc(total);
+    }
+
+    const fecthExpenses = async () => {
+      let totalExp = 0;
+      const expResp = await axios.get("http://localhost:5000/api/v1/get-expenses");
+      const exp = expResp.data;
+      exp.forEach((key)=>{
+        totalExp = totalExp + key.amount;
+      })
+      setExp(totalExp);
+    }
+
+    fecthExpenses();
+    fetchIncomes();
+  }, []);
+
   return (
     <>
       <section className='cards'>
         <div className='cardBox balances'>
           <Common title='Total Balance' />
           <div className='circle'>
-            <h1><CurrencyRupeeIcon /> {totalIncome-totalExpense}</h1>
+            <h1><CurrencyRupeeIcon /> {inc - exp}</h1>
           </div>
         </div>
         <div className='cardBox balances'>
           <Common title='Total Income' />
           <div className='circle'>
-            <h1><CurrencyRupeeIcon /> {totalIncome}</h1>
+            <h1><CurrencyRupeeIcon /> {inc}</h1>
           </div>
         </div>
         <div className='cardBox balances'>
           <Common title='Total Expenses' />
           <div className='circle'>
-            <h1><CurrencyRupeeIcon /> {totalExpense}</h1>
+            <h1><CurrencyRupeeIcon /> {exp}</h1>
           </div>
         </div>
       </section>
