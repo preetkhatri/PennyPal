40eadb519f342e1953453af9f8015879976116de
preetkhatri <preetkhatri07@gmail.com>
2024-04-18 01:12:46 -0700

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css"
 import Income from "./components/income/Income"
 import SignUp from "./components/signup/SignUp";
+import Login from "./components/login/Login";
 import Expense from "./components/expense/Expense";
 import { IncomeContext } from "./Context/IncomeContext";
 import Dashboard from "./components/dashboard/Dashboard"
@@ -18,10 +19,11 @@ function App() {
         <IncomeContext.Provider value={{ totalIncome, setTotalIncome }}>
           <BrowserRouter>
             <Routes>
-              <Route exact path="/signup" element={<SignUp/>}/>
               <Route exact path="/" element={<Dashboard />} />
               <Route exact path="/incomes" element={<Income />} />
               <Route exact path="/expenses" element={<Expense />} />
+              <Route exact path="/signup" element={<SignUp/>}/>
+              <Route exact path="/login" element={<Login/>}/>
             </Routes>
           </BrowserRouter>
         </IncomeContext.Provider>
---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const defaultTheme = createTheme();
+
+export default function Login() {
+    const [loginData, setLoginData] = React.useState({
+        username: "",
+        password: ""
+    });
+
+    function handleChange(event) {
+        setLoginData((prevdata) => {
+            return {
+                ...prevdata,
+                [event.target.name]: event.target.value
+            };
+        });
+    }
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={(e) => { console.log(loginData); e.preventDefault(); }} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoFocus
+                            onChange={handleChange}
+                            value={loginData.username}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            onChange={handleChange}
+                            value={loginData.password}                            
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
